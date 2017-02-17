40a72aff603aa1d395e83931899236088c8faa4c
ralam4 <ralam4@asu.edu>
2017-02-17 10:28:31 -0700

    First website upload

---
+++ contact.php
@@ -0,0 +1,22 @@
+<?php
+
+if(isset($_POST['submit'])) {
+   // Prepare the email
+$to = 'ralam70@gmail.com';
+
+$name = $_POST['name'];
+$mail_from = $_POST['email'];
+   $subject = 'Message sent from projectx5w1h';
+   $message = $_POST['message'];
+
+$header = "From: $name <$mail_from>";
+
+   // Send it
+   $sent = mail($to, $subject, $message, $header);
+   if($sent) {
+   echo 'Your message has been sent successfully!';
+   } else {
+   echo 'Sorry, your message could not send.';
+   }
+}
+?>
