b0450e4c612be9f381d2d29ed71dc4467c10a6f3
ralam4 <ralam4@asu.edu>
2017-02-18 01:10:23 -0700

    contact8

--- contact.php
+++ contact.php
@@ -1,22 +1,44 @@
 <?php
+/* Set e-mail recipient */
+$myemail = "ralam70@gmail.com";
 
-if(isset($_POST["submit"])) {
-   // Prepare the email
-$to = "ralam70@gmail.com";
+/* Check all form inputs using check_input function */
+$name = check_input($_POST['name'], "Enter your name");
+$email = check_input($_POST['email']);
+$message = check_input($_POST['message'], "Write your message");
 
-$name = $_POST["name"];
-$mail_from = $_POST["email"];
-   $subject = 'Message sent from Project 5W1H';
-   $message = $_POST["message"];
-
-$header = "From: $name <$mail_from>";
-
-   // Send it
-   $sent = mail($to, $subject, $message, $header);
-   if($sent) {
-   echo "Your message has been sent successfully!";
-   } else {
-   echo "Sorry, your message could not send.";
-   }
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("E-mail address not valid");
 }
+/* Let's prepare the message for the e-mail */
+$message = "
+
+Name: $name
+E-mail: $email
+Subject: $subject
+
+Message:
+$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $message);
+
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
 ?>
